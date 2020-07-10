48578182c495bf9196257010f980dd177103bdb6
Tom Coffee <thetomcoffee@users.noreply.github.com>
2020-07-10 12:28:50 -0500

    First Commit
    First commit. Half -way through MVC 5 tutorial. At "Accessing Your Model's Data from a Controller"  https://docs.microsoft.com/en-us/aspnet/mvc/overview/getting-started/introduction/accessing-your-models-data-from-a-controller

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/ 
+
+        //ver 1:
+        //public string Index()
+        //{
+        //    return "This is my <b>default</b> action...";
+        //}
+
+        //ver 2:
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        //ver 1:
+        //public string Welcome(string name, int ID = 1)
+        //{
+        //    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
+        //}
+
+        //ver 2:
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
+    }
+}
